9449dc69fc025e5075a11bad06a60ea0636897fc
cderamond <cderamond@gmail.com>
2015-03-18 18:16:41 -0300

    SVD for the win

--- proj1_sparseCode.R
+++ proj1_sparseCode.R
@@ -26,10 +26,36 @@ data$am_f <- as.factor(data$am)
 
 
 
-##
+## eda advanced
 source("./myplclust.R")
 d <- dist(data)
 clust <- hclust(d)
 myplclust(clust,lab.col=data$am)
 
-svd1 <- svd(data)
+###
+# svd
+svd1 <- svd(scale(data[,-1]))
+# get 3 top contributor
+#maxContrib <- which.max(abs(svd1$v[,2])) no need for the max
+top3Contrib <- sort(abs(svd1$v[,2]), 
+                    decreasing = T, 
+                    index.return=T)$ix [1:3] #straight from Leek professor, we take 3 top contributors
+
+# graph for % of variation explained
+par(mfrow = c(1, 2))
+plot(svd1$d, xlab = "Column", ylab = "Singular value", pch = 19)
+plot(svd1$d^2/sum(svd1$d^2), xlab = "Column", ylab = "Prop. of variance explained", 
+     pch = 19)
+#########
+d <- dist(data[,top3Contrib])#
+clust <- hclust(d)
+plot(clust)
+
+## we now know that by taking the top 3 contributor we should explain pVarEx %
+pVarEx <- sum((svd1$d^2/sum(svd1$d^2))[1:3]) #round it to 2 decimals *100 
+
+# k-means
+kClust <- kmeans(data[,-1],centers=2, nstart = 10)
+table(kClust$cluster,data$am)
+
+
